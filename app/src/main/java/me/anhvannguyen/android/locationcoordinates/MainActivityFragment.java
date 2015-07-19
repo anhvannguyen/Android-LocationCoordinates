1416ed77f6069e3e89cfcc3be2faaa8279bf0f2d
Anh Nguyen <nguyen.anhvan@gmail.com>
2015-07-18 17:20:39 -0700

    add member variables

--- app/src/main/java/me/anhvannguyen/android/locationcoordinates/MainActivityFragment.java
+++ app/src/main/java/me/anhvannguyen/android/locationcoordinates/MainActivityFragment.java
@@ -1,16 +1,28 @@
 package me.anhvannguyen.android.locationcoordinates;
 
+import android.location.Location;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
 
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+    private static final String LOG_TAG = MainActivityFragment.class.getSimpleName();
+
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+    private Location mLastLocation;
+    private TextView mLatTextView;
+    private TextView mLongTextView;
 
     public MainActivityFragment() {
     }
@@ -18,6 +30,8 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        return rootView;
     }
 }
